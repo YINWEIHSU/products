30dd8976d9313db475d7f1e42b188c87e78a96d1
Jerry Hsu <cba5530@gmail.com>
2020-08-12 22:25:32 +0800

    check file exist

--- products.py
+++ products.py
@@ -1,12 +1,22 @@
-#讀取檔案
+import os
+
+#找檔案
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #繼續
-		name, price = line.strip().split(',') #遇到逗點就切
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('yes')
+	#讀取檔案
+
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續
+			name, price = line.strip().split(',') #遇到逗點就切
+			products.append([name, price])
+	print(products)
+else:
+	print('no')
+
+
 
 #使用者輸入
 while True:
